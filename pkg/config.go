e4fc78ad43cf2227218b45929c0f49215a853109
Ricky Alturino <alturino001@gmail.com>
2024-10-15 12:07:07 +0700

    chore(main): remove directory config, log, postgres from pkg directory

--- pkg/config.go
+++ pkg/config.go
@@ -1,11 +1,9 @@
-package config
+package pkg
 
 import (
 	"time"
 
 	"github.com/spf13/viper"
-
-	"github.com/Alturino/url-shortener/pkg/log"
 )
 
 type Config struct {
@@ -17,28 +15,28 @@ type Database struct {
 	Host           string `mapstructure:"host"`
 	DbName         string `mapstructure:"name"`
 	Password       string `mapstructure:"password"`
-	Port           uint16 `mapstructure:"port"`
 	Username       string `mapstructure:"username"`
 	MigrationPath  string `mapstructure:"migration_path"`
+	TimeZone       string `mapstructure:"timezone"`
+	Port           uint16 `mapstructure:"port"`
 	MaxConnections byte   `mapstructure:"max_connections"`
 	MinConnections byte   `mapstructure:"min_connections"`
-	TimeZone       string `mapstructure:"timezone"`
 }
 
 func InitConfig(filename string) Config {
 	startTime := time.Now()
 
 	config := Config{}
-	log.Logger.Info().
-		Str(log.KeyProcess, "InitConfig").
-		Time(log.KeyStartTime, startTime).
+	Logger.Info().
+		Str(KeyProcess, "InitConfig").
+		Time(KeyStartTime, startTime).
 		Msg("starting InitConfig")
 	defer func() {
-		log.Logger.Info().
-			Str(log.KeyProcess, "InitConfig").
-			Dur(log.KeyProcessingTime, time.Since(startTime)).
-			Time(log.KeyEndTime, time.Now()).
-			Time(log.KeyStartTime, startTime).
+		Logger.Info().
+			Str(KeyProcess, "InitConfig").
+			Dur(KeyProcessingTime, time.Since(startTime)).
+			Time(KeyEndTime, time.Now()).
+			Time(KeyStartTime, startTime).
 			Interface("config", config).
 			Msg("finished InitConfig")
 	}()
@@ -49,18 +47,18 @@ func InitConfig(filename string) Config {
 
 	err := viper.ReadInConfig()
 	if err != nil {
-		log.Logger.Err(err).
+		Logger.Err(err).
 			Str("filename", filename).
-			Str(log.KeyProcess, "InitConfig").
-			Dur(log.KeyProcessingTime, time.Since(startTime)).
+			Str(KeyProcess, "InitConfig").
+			Dur(KeyProcessingTime, time.Since(startTime)).
 			Msgf("error when reading config with error=%s", err.Error())
 	}
 
 	err = viper.Unmarshal(&config)
 	if err != nil {
-		log.Logger.Err(err).
-			Str(log.KeyProcess, "InitConfig").
-			Dur(log.KeyProcessingTime, time.Since(startTime)).
+		Logger.Err(err).
+			Str(KeyProcess, "InitConfig").
+			Dur(KeyProcessingTime, time.Since(startTime)).
 			Msgf("error unmarshaling config with error=%s", err.Error())
 	}
 
